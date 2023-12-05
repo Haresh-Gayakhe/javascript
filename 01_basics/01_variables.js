5159f62d4ea0a9eaef7dfa621a12b94200fe8676
Haresh Gayakhe <75369186+Haresh-Gayakhe@users.noreply.github.com>
2023-12-05 16:28:04 +0000

    let const var

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 123;
+let email = "demo@gmail.com";
+var name = "Hitesh"; // Don't use var 
+accountCity = "Bhiwandi";
+
+// accountId = 1234 (Not allowed)
+email = "test@gmail.com"
+name = "Haresh"
+accountCity = "Kalyan"
+
+console.table([accountId,email,name,accountCity]);
