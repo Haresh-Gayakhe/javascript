c5b6d63c29debc09d747a78622e82011b78ae2c6
Haresh Gayakhe <hareshgayakhe5421@gmail.com>
2023-12-09 21:28:55 +0530

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+// alert("Hello"); This will not work in node 
+
+console.log(3 
+    +
+     3); // don't use like this 
+
+
+     
+let name = "Haresh"
+console.log(typeof(name))
+let age = 26
+console.log(typeof(age))
+let isAlive = true
+console.log(typeof(isAlive))
+let value = null
+console.log(typeof(value))
+let notAvailable = undefined
+console.log(typeof(notAvailable))
+let bigInt = BigInt("100")
+console.log(typeof(bigInt))
+let object = {name:"Haresh", age:26}
+console.log(typeof(object))
+let symbol = Symbol("symbol")
+console.log(typeof(symbol))
