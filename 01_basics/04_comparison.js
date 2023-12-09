d82b73fadc4c8b2ed64b48ae3faeed826c7936b3
Haresh Gayakhe <hareshgayakhe5421@gmail.com>
2023-12-09 23:07:38 +0530

    comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+console.log(2 > 1)
+console.log(2 < 1)
+console.log(2 == 1)
+
+console.log("2" == 2) // normal comparison
+console.log("2" === 2) // strict comparison - it compares data types also
+
+// avoid below comparisons
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+console.log(undefined == 0)
+console.log(undefined > 0)
+console.log(undefined < 0)
