39c6cf763bfbeae41d61d31a4dba943f67f78638
Haresh Gayakhe <hareshgayakhe5421@gmail.com>
2023-12-09 22:48:08 +0530

    operatins

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -1,6 +1,22 @@
+//***************** Conversions *****************
 let score = 0
-console.log(typeof(score))
+// console.log(typeof(score))
 
 let valueInNumber = Boolean(score);
-console.log(typeof(valueInNumber));
-console.log(valueInNumber)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber)
+
+//***************** Operations *****************
+let value = 3;
+let negValue = -value
+console.log(negValue)
+
+let str = "Hello ";
+let str2 = "Haresh";
+let str3 = str + str2;
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
