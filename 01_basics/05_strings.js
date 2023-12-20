f6cd229987bded3208ed1e7d6797d1dc3d292468
Haresh Gayakhe <hareshgayakhe5421@gmail.com>
2023-12-20 16:36:55 +0530

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+const me = "Haresh";
+const age = 26;
+console.log("me is " + me + " and age is " + age + " years.");
+
+console.log(`me is ${me} and age is ${age} years`);
+
+console.log(me.length);
+console.log(me.toUpperCase());
+console.log(me.concat(" Gayakhe"));
+
+let str = "my name is haresh gayakhe    "
+
+console.log(str.trim().replace("haresh","Haresh"))
+console.log(str.includes("har"))
+
+console.log(str.trim().split(" "))
+console.log(str.slice(10,15))
+console.log(str.valueOf())
