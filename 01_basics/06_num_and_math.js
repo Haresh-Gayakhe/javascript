95bdbd36afecdf550a1d76d49dc293f8b2e95857
Haresh Gayakhe <hareshgayakhe5421@gmail.com>
2023-12-23 11:36:33 +0530

    number and math

---
+++ 01_basics/06_num_and_math.js
@@ -0,0 +1,27 @@
+const score = 600;
+
+const balance = new Number(100);
+
+// console.log(balance)
+
+let value = 1000000000
+// console.log(value.toFixed(2))
+// console.log(value.toPrecision(3))
+// console.log(value.toLocaleString())
+
+
+
+// ++++++++++++ Maths +++++++++++++
+console.log(Math)
+console.log(Math.abs(-150))
+console.log(Math.PI)
+console.log(Math.round(3.1))
+console.log(Math.round(3.5))
+console.log(Math.ceil(3.1))
+console.log(Math.floor(3.9))
+console.log(Math.sqrt(25))
+console.log(Math.pow(2,3))
+
+console.log((Math.random() * 10 + 1).toFixed(0))
+
+
